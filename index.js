7b3e3cc7d887cb3b8f09584c8a0237903b723602
Jignesh Mangarolia <jigneshmangarolia@Jigneshs-MBP.home>
2021-06-13 01:08:11 -0500

    Importing Codebase

---
+++ index.js
@@ -0,0 +1,22 @@
+// let today = new Date();
+// let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.console.log(getDate());
+// var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+// var dateTime = date + ' ' + time;
+const { DateTime } = require(“luxon”);
+
+let time = document.getElementById('time-id').innerHTML;
+
+function getTime() {
+  let refresh = 1000; // Refresh rate in milliseconds
+  let myTime = setTimeout('display_Time()', refresh);
+}
+function display_Time() {
+  let cTime = new Date(); // Can use this for month, day, year, hours, minutes, and seconds.
+  let currentTime = cTime.getHours() + ':' + cTime.getMinutes() + ':' + cTime.getSeconds();
+  // let currentDate = cTime.toLocaleString('en-GB');
+  let cDate = cTime.toLocaleString('en-US', { timeZone: 'America' });
+  console.log(currentTime);
+  document.getElementById('date-id').innerHTML = cDate;
+  document.getElementById('hi-id').innerHTML = currentTime;
+  getTime();
+}
